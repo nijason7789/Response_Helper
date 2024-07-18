2ed13f8e8fcc68ef213be57de72a8ed5d1340af3
nijason7789 <nijason7789@gmail.com>
2024-07-18 22:42:08 +0800

    upload FE UI for Default page and Recommand select page

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,15 @@
-console.log("popup.js has been loaded successfully.");
-document.addEventListener('DOMContentLoaded', function() {
-    let colorChangeBtn = document.getElementById('changeColor');
-    colorChangeBtn.addEventListener('click', function() {
-        let contentArea = document.getElementById('contentArea');
-        contentArea.style.backgroundColor = 'blue';
-    });
+document.getElementById('sendButton').addEventListener('click', function() {
+    document.getElementById('step1').style.display = 'none';
+    document.getElementById('step2').style.display = 'flex'; // 使用 flex 来保持按钮垂直居中
 });
 
+document.getElementById('returnButton').addEventListener('click', function() {
+    document.getElementById('step2').style.display = 'none';
+    document.getElementById('step1').style.display = 'flex'; // 返回第一步骤
+});
+
+// 可以为 'More' 按钮添加额外的事件监听器
+document.getElementById('moreButton').addEventListener('click', function() {
+    console.log("More button clicked");
+    // 这里可以添加更多的功能，如加载更多选项
+});

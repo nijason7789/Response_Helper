5382274d977ff53bfa36d59a4679c3674b791851
nijason7789 <nijason7789@gmail.com>
2024-07-25 21:57:33 +0800

    finish copy suggestion function.

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,19 @@
-import { handleCopyButtonClick, handleSendButtonClick, handleReturnButtonClick, handleMoreButtonClick } from "./scripts/buttonEvents.js";
+import { handleCopyButtonClick, handleSendButtonClick, handleReturnButtonClick, handleMoreButtonClick, handleSuggestionBtnClick } from "./scripts/buttonEvents.js";
 import { setupMessageListener } from './scripts/messageHandler.js'
-const copyBtn = document.getElementById('copyButton')
-const sendBtn = document.getElementById('sendButton')
-const returnBtn = document.getElementById('returnButton')
-const moreBtn = document.getElementById('moreButton')
+const copyBtn = document.getElementById('copyButton');
+const sendBtn = document.getElementById('sendButton');
+const returnBtn = document.getElementById('returnButton');
+const moreBtn = document.getElementById('moreButton');
+const suggestionBtns = document.querySelectorAll('.actionButton');
 
 copyBtn.addEventListener('click', handleCopyButtonClick);
 sendBtn.addEventListener('click', handleSendButtonClick);
 returnBtn.addEventListener('click',handleReturnButtonClick);
 moreBtn.addEventListener('click', handleMoreButtonClick);
+
+suggestionBtns.forEach(btn => {
+    btn.addEventListener('click', handleSuggestionBtnClick);
+});
 
 setupMessageListener();
 

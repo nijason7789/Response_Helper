6e5e1768732a2e963bf68926b7d8b1f446f14131
nijason7789 <nijason7789@gmail.com>
2024-07-28 15:59:58 +0800

    add welcome page and send wakeup API

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,11 @@
-import { handleSendButtonClick, handleReturnButtonClick, handleMoreButtonClick, handleSuggestionBtnClick } from "./scripts/buttonEvents.js";
+import { handleWelcomeButtonClick, handleSendButtonClick, handleReturnButtonClick, handleMoreButtonClick, handleSuggestionBtnClick } from "./scripts/buttonEvents.js";
+const welcomeBtn = document.getElementById('welcomeButton');
 const sendBtn = document.getElementById('sendButton');
 const returnBtn = document.getElementById('returnButton');
 const moreBtn = document.getElementById('moreButton');
 const suggestionBtns = document.querySelectorAll('.actionButton');
 
+welcomeBtn.addEventListener('click',handleWelcomeButtonClick);
 sendBtn.addEventListener('click', handleSendButtonClick);
 returnBtn.addEventListener('click',handleReturnButtonClick);
 moreBtn.addEventListener('click', handleMoreButtonClick);
--- src/scripts/api/sendApi.js
+++ src/scripts/api/sendApi.js
@@ -41,3 +41,23 @@ export function sendCommentRequest(commentInput, callback){
       callback(error);
     });
   }
+
+  export function sendWakeUpRequest(callback){
+    const api_url = config.API_DOMAIN;
+    const api_path = '/api/wakeup';
+    fetch(api_url+api_path, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      callback(error);
+    });
+  }
